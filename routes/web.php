bcccdb73d05f03ba6dffd5956b907b90a07d7843
mmartinslovik <martinslovik1122@gmail.com>
2022-05-25 20:30:14 +0200

    edit welcome route on middleware auth

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['auth'])->name('welcome');
 
 Route::get('/export', function () {
     return view('export');
